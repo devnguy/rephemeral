6d240b58bc3314669f11564973e80426fcf138d3
devnguy <hdn301@gmail.com>
2026-01-01 16:57:35 -0600

    [DEV-34] Dark mode theme select needed a system option

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { H3 } from "@/components/ui/typography";
 import { auth, signIn, signOut } from "@/auth";
-import { ThemeToggleButton } from "../theme/toggle-button";
+import { ThemeSelect } from "../theme/toggle-button";
 
 export async function Header() {
   const session = await auth();
@@ -18,7 +18,7 @@ export async function Header() {
       </div>
       <div className="flex items-center space-x-2">
         {!session?.user ? <SignInButton /> : <SignOutButton />}
-        <ThemeToggleButton />
+        <ThemeSelect />
       </div>
     </div>
   );
