87ada88345548d8d47ccf7fba93a4ee4a92209fa
devnguy <hdn301@gmail.com>
2025-12-31 18:56:25 -0600

    [DEV-26] Keyboard controls

--- components/drawing-session/controller.tsx
+++ components/drawing-session/controller.tsx
@@ -8,24 +8,59 @@ import {
   Square,
 } from "lucide-react";
 import { useDrawingSessionContext } from "./context";
+import { useCallback, useEffect } from "react";
 
 export function Controller() {
   const { state, dispatch } = useDrawingSessionContext();
-  const handleForward = () => {
+  const handleForward = useCallback(() => {
     dispatch({ type: "FORWARD" });
-  };
+  }, [dispatch]);
 
-  const handleBack = () => {
+  const handleBack = useCallback(() => {
     dispatch({ type: "BACK" });
-  };
+  }, [dispatch]);
 
-  const handleTogglePause = () => {
+  const handleTogglePause = useCallback(() => {
     dispatch({ type: "TOGGLE_PAUSE" });
-  };
+  }, [dispatch]);
 
-  const handleStop = () => {
+  const handleStop = useCallback(() => {
     dispatch({ type: "STOP" });
-  };
+  }, [dispatch]);
+
+  const handleKeyUp = useCallback(
+    (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          e.preventDefault();
+          handleBack();
+          return;
+        case "ArrowRight":
+          e.preventDefault();
+          handleForward();
+          return;
+        case " ":
+          e.preventDefault();
+          handleTogglePause();
+          return;
+        case "Escape":
+          e.preventDefault();
+          handleStop();
+          return;
+        default:
+          return;
+      }
+    },
+    [handleBack, handleForward, handleTogglePause, handleStop],
+  );
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [handleKeyUp]);
 
   return (
     <div className="flex space-x-4">
