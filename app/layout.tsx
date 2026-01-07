acac802314d2f3b33e19a158baec29790b7ce493
devnguy <hdn301@gmail.com>
2026-01-07 09:45:01 -0600

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@/components/theme/theme-provider";
@@ -42,6 +43,7 @@ export default function RootLayout({
             </DrawingSessionContextProvider>
           </SessionProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
